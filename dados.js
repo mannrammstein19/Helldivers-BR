c96b5c0a1f3830e1197bc660dd7fafe986f1a78c
mannrammstein19 <mannrammstein@gmail.com>
2026-06-22 18:54:52 -0300

    update

--- dados.js
+++ dados.js
@@ -1,10 +1,34 @@
 // dados.js - Banco de dados local da sua Wiki
 const searchData = [
     {
-        title: "Laser Orbital",
-        category: "Estratagema",
+        title: "Metralhadora",
+        category: "Arma de Apoio",
         url: "estratagemas.html",
-        icon: "imagens/2-estratagemas/2-orbitais/Orbital_Laser_Stratagem_Icon_Background.svg" 
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora.svg" 
+    },
+	    {
+        title: "Antitanque Descartável",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Antitanque Descartável.svg" 
+    },
+	    {
+        title: "Vigorosa",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Vigorosa.svg" 
+    },
+	    {
+        title: "Canhão a Laser",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Canhão a Laser.svg" 
+    },
+	    {
+        title: "Metralhadora",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Rifle Antimaterial.svg" 
     },
     {
         title: "Rifle Sem Recuo",
@@ -14,7 +38,7 @@ const searchData = [
     },
     {
         title: "Vigorosa",
-        category: "Armadura",
+        category: "Arma de Apoio",
         url: "estratagemas.html",
         icon: "imagens/armaduras/vigorosa.png" 
     },
@@ -23,5 +47,18 @@ const searchData = [
         category: "Arma de Apoio",
         url: "estratagemas.html",
         icon: "imagens/2-estratagemas/canhao-automatico.png" 
-    }
+    },
+	    {
+        title: "Canhão Automático",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/canhao-automatico.png" 
+    },
+	    {
+        title: "Laser Orbital",
+        category: "Estratagema",
+        url: "estratagemas/1-orbitais/laser-orbital/index.html",
+        icon: "imagens/2-estratagemas/2-orbitais/Orbital_Laser_Stratagem_Icon_Background.svg" 
+    },
+
 ];

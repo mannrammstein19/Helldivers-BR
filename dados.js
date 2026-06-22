5af280d691449022d5a6efca2f305537df254d0e
mannrammstein19 <mannrammstein@gmail.com>
2026-06-22 12:40:28 -0300

    update

---
+++ dados.js
@@ -0,0 +1,27 @@
+// dados.js - Banco de dados local da sua Wiki
+const searchData = [
+    {
+        title: "Laser Orbital",
+        category: "Estratagema",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/2-orbitais/Orbital_Laser_Stratagem_Icon_Background.svg" 
+    },
+    {
+        title: "Rifle Sem Recuo",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/rifle-sem-recuo.png" 
+    },
+    {
+        title: "Vigorosa",
+        category: "Armadura",
+        url: "estratagemas.html",
+        icon: "imagens/armaduras/vigorosa.png" 
+    },
+    {
+        title: "Canhão Automático",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/canhao-automatico.png" 
+    }
+];

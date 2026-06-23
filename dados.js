e6e5f3f627208987fe70f87cc75026ee1bb22870
mannrammstein19 <152840160+mannrammstein19@users.noreply.github.com>
2026-06-23 03:14:42 -0300

    Atualizar o dados.js

--- dados.js
+++ dados.js
@@ -54,6 +54,114 @@ const searchData = [
         url: "estratagemas.html",
         icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora Pesada.svg" 
     },
+    {
+        title: "Canhão Automático",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora.svg" 
+    },
+	    {
+        title: "Lançador de Arco",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Antitanque Descartável.svg" 
+    },
+	    {
+        title: "Canhão Quasar",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Vigorosa.svg" 
+    },
+	    {
+        title: "Lança Foguetes de Explosão Atmosférica",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Canhão a Laser.svg" 
+    },
+	    {
+        title: "Comando",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Rifle Antimaterial.svg" 
+    },
+    {
+        title: "Lança",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/lancador de granadas.svg" 
+    },
+    {
+        title: "Railgun",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Rifle sem Recuo.svg" 
+    },
+    {
+        title: "Lança Mísseis V.E.S.P.A",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Lança-chamas.svg" 
+    },
+	    {
+        title: "Martelo Rompedor",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora Pesada.svg" 
+    },
+    {
+        title: "Época",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora.svg" 
+    },
+	    {
+        title: "Tempestade de Balas",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Antitanque Descartável.svg" 
+    },
+	    {
+        title: "Arbalete",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Vigorosa.svg" 
+    },
+	    {
+        title: "Ferramenta de Desfolhamento",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Canhão a Laser.svg" 
+    },
+	    {
+        title: "Esterilizador",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Rifle Antimaterial.svg" 
+    },
+    {
+        title: "Napalm Descartável",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/lancador de granadas.svg" 
+    },
+    {
+        title: "Desobstrutora",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Rifle sem Recuo.svg" 
+    },
+    {
+        title: "Desescalador",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Lança-chamas.svg" 
+    },
+	    {
+        title: "Lança Granadas com Fitas",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "imagens/2-estratagemas/1-armas-de-apoio/Metralhadora Pesada.svg" 
+    },
 	    {
         title: "Laser Orbital",
         category: "Estratagema",

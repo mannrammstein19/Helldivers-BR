07b971b9baf6dc1710b643fdeae876151191a3d5
mannrammstein19 <mannrammstein@gmail.com>
2026-09-15 22:43:59 -0300

    update

---
+++ warbonds.js
@@ -0,0 +1,63 @@
+// dados.js - Banco de dados local da sua Wiki
+const warbondsData = [
+    {
+        title: "Senador",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/P-4_Senator_Secondary_Render.png" 
+    },
+	    {
+        title: "AR-23C Liberator Concussive",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/AR-23C_Liberator_Concussive_Primary_Render.png" 
+    },
+	    {
+        title: "Soldado de Aço SA-25",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/SA-25_Steel_Trooper_Armor_Render.png" 
+    },
+	    {
+        title: "Soldado de Aço SA-25",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/SA-25_Steel_Trooper_Helmet_Render.png" 
+    },
+	    {
+        title: "G-10 Incendiary",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-2/G-10_Incendiary_Throwable_Render.png" 
+    },
+    {
+        title: "SG-225IE Breaker Incendiary",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-2/SG-225IE_Breaker_Incendiary_Primary_Render.png" 
+    },
+    {
+        title: "SA-12 Servo Assisted",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-2/SA-12_Servo_Assisted_Armor_Render.png" 
+    },
+    {
+        title: "JAR-5 Dominator",
+        category: "Arma de Apoio",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-3/JAR-5_Dominator_Primary_Render.png" 
+    },
+	    {
+        title: "SA-32 Dynamo",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-3/SA-32_Dynamo_Armor_Render.png" 
+    },
+    {
+        title: "Bastion of Integrity",
+        category: "Steeled Veterans",
+        url: "estratagemas.html",
+        icon: "https://pub-f324221f4e5e42b08ecfa5062afd5960.r2.dev/helldivers-br/warbonds/steeled_veterans/images/pagina-3/Bastion_of_Integrity_Cape_Render.png" 
+    },
+];
